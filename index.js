bfbeea67a1e2190016d9c8c6295d9338f5f78af0
yousef271 <128397039+yousef271@users.noreply.github.com>
2023-12-11 15:50:10 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+function arrow_right() {
+    document.getElementById("first").style.display = "none";
+    document.getElementById("second").style.display = "block";
+}
+
+function arrow_left() {
+    document.getElementById("first").style.display = "block";
+    document.getElementById("second").style.display = "none";
+}
+
+function arrow_right1() {
+    document.getElementById("third").style.display = "block";
+    document.getElementById("second").style.display = "none";
+}
+
+function arrow_right2() {
+    document.getElementById("forth").style.display = "block";
+    document.getElementById("third").style.display = "none";
+}
+
+function arrow_left1() {
+    document.getElementById("third").style.display = "none";
+    document.getElementById("second").style.display = "block";
+}
+
+function arrow_left2() {
+    document.getElementById("forth").style.display = "none";
+    document.getElementById("third").style.display = "block";
+}
+
+function arrow_right3() {
+    document.getElementById("forth").style.display = "none";
+    document.getElementById("fifth").style.display = "block";
+}
+
+function arrow_right4() {
+    document.getElementById("fifth").style.display = "none";
+    document.getElementById("sixth").style.display = "block";
+}
+
+function arrow_left3() {
+    document.getElementById("forth").style.display = "block";
+    document.getElementById("fifth").style.display = "none";
+}
+
+function arrow_left4() {
+    document.getElementById("fifth").style.display = "block";
+    document.getElementById("sixth").style.display = "none";
+}
+
+function arrow_right5() {
+    document.getElementById("sixth").style.display = "none";
+    document.getElementById("seventh").style.display = "block";
+}
+
+function arrow_left5() {
+    document.getElementById("sixth").style.display = "block";
+    document.getElementById("seventh").style.display = "none";
+}
