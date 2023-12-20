15bd20db12c597753ce44da2d9efa4c7fed167f6
yousef271 <128397039+yousef271@users.noreply.github.com>
2023-12-20 14:02:33 +0400

    their will be more this is a test please don't use the webiste now because for iphone  android and xiaomi does not support for this website

--- index.js
+++ index.js
@@ -76,14 +76,32 @@ const operatingSystem = getOperatingSystem();
 
 if (operatingSystem === "iOS") {
   console.log("this is an iphone");
-  document.getElementById("iphone_operating_section").style.display = "block";
+  document.getElementById("first_iphone").style.display = "block";
   document.getElementById("windows").style.display = "none";
 } else if (operatingSystem === "Android") {
     console.log("this is an Android");
-    document.getElementById("iphone_operating_section").style.display = "block";
+    document.getElementById("first_iphone").style.display = "block";
     document.getElementById("windows").style.display = "none";
 } else if (operatingSystem === "Xiaomi") {
     console.log("this is an Xiaomi");
-    document.getElementById("iphone_operating_section").style.display = "block";
+    document.getElementById("first_iphone").style.display = "block";
     document.getElementById("windows").style.display = "none";
 }
+
+function getOperatingSystem() {
+    const userAgent = navigator.userAgent;
+  
+    if (userAgent.includes("Windows")) {
+        document.getElementById("iphone_operating_section").style.display = "none";
+        document.getElementById("windows").style.display = "block";
+    } else if (userAgent.includes("Mac OS")) {
+        document.getElementById("iphone_operating_section").style.display = "none";
+        document.getElementById("first").style.display = "block";
+      return "Mac OS";
+    } else if (userAgent.includes("Linux")) {
+        document.getElementById("iphone_operating_section").style.display = "none";
+        document.getElementById("first").style.display = "block";
+      return "Linux";
+  }
+}
+  const operatingSystem = getOperatingSystem();
