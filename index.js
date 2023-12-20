cb644a40ec761ec3bc7e8cdf9e2b0e61aaf3a508
yousef271 <128397039+yousef271@users.noreply.github.com>
2023-12-20 13:45:44 +0400

    testing

--- index.js
+++ index.js
@@ -57,3 +57,34 @@ function arrow_left5() {
     document.getElementById("sixth").style.display = "block";
     document.getElementById("seventh").style.display = "none";
 }
+
+function getOperatingSystem() {
+  const userAgent = navigator.userAgent;
+  
+  if (userAgent.includes("iOS")) {
+    return "iOS";
+  } else if (userAgent.includes("Android")) {
+    return "Android";
+  } else if (userAgent.includes("Xiaomi")) {
+    return "Xiaomi";
+  } else {
+    return "Unknown";
+  }
+}
+
+const operatingSystem = getOperatingSystem();
+
+if (operatingSystem === "iOS") {
+  console.log("iOS section");
+  document.getElementById("iphone_operating_section").style.display = "block";
+  document.getElementById("windows").style.display = "none";
+} else if (operatingSystem === "Android") {
+  // Display Android-specific section
+  console.log("Android section");
+} else if (operatingSystem === "Xiaomi") {
+  // Display Xiaomi-specific section
+  console.log("Xiaomi section");
+} else {
+  // Display default section or handle unknown OS
+  console.log("Unknown or unsupported operating system");
+}
